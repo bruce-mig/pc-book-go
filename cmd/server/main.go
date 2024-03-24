679b1085814f678c1adae7e944975fd85acdf3cb
bruce-mig <brucetmig@icloud.com>
2024-03-24 21:40:07 +0200

    added gRPC server unary and stream auth interceptors

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,24 +5,77 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
+	"github.com/joho/godotenv"
 	"gitlab.com/brucemig/pcbook/pb"
 	"gitlab.com/brucemig/pcbook/service"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
+func seedUsers(userStore service.UserStore) error {
+	if err := createUser(userStore, "admin1", "secret", "admin"); err != nil {
+		return err
+	}
+
+	return createUser(userStore, "user1", "secret", "user")
+}
+
+func createUser(userStore service.UserStore, username, password, role string) error {
+	user, err := service.NewUser(username, password, role)
+	if err != nil {
+		return err
+	}
+	return userStore.Save(user)
+}
+
+// TODO: use .env file instead
+const (
+	secretKey     = "secret"
+	tokenDuration = 15 * time.Minute
+)
+
+func accessibleRoles() map[string][]string {
+	const laptopServicePath = "/brucemig.pcbook.LaptopService/"
+	return map[string][]string{
+		laptopServicePath + "CreateLaptop": {"admin"},
+		laptopServicePath + "UploadImage":  {"admin"},
+		laptopServicePath + "RateLaptop":   {"admin", "user"},
+	}
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
 	port := flag.Int("port", 0, "the server port")
 	flag.Parse()
 	log.Printf("start server on port %d", *port)
+
+	userStore := service.NewInMemoryUserStore()
+	if err := seedUsers(userStore); err != nil {
+		log.Fatal("cannot seed users")
+	}
+
+	jwtManager := service.NewJWTManager(secretKey, tokenDuration)
+	authServer := service.NewAuthServer(userStore, jwtManager)
 
 	laptopStore := service.NewInMemoryLaptopStore()
 	imageStore := service.NewDiskImageStore("img")
 	ratingStore := service.NewInMemoryRatingStore()
 
 	laptopServer := service.NewLaptopServer(laptopStore, imageStore, ratingStore)
-	grpcServer := grpc.NewServer()
+
+	interceptor := service.NewAuthInterceptor(jwtManager, accessibleRoles())
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(interceptor.Unary()),
+		grpc.StreamInterceptor(interceptor.Stream()),
+	)
+
+	pb.RegisterAuthServiceServer(grpcServer, authServer)
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
 	reflection.Register(grpcServer)
 
